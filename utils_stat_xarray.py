7e38ca0593a5b8d4e3e207fd57ae3f691e6c7b00
Limbert Torrez R <limbert.torrez@userena.cl>
2024-12-09 18:42:42 -0300

    primera version climate xarray tools lifer

--- utils_stat_xarray.py
+++ utils_stat_xarray.py
@@ -8,7 +8,7 @@ import pymannkendall as mk
 
 def xr_mktrend(da,dim = 'time',sig = 0.05):
     '''
-    This function compute some paramters for statitical trend in time series.    
+    This function compute some paramters for statistical trend in time series.    
 
     Parameters
     ----------
@@ -61,6 +61,23 @@ def xr_mktrend(da,dim = 'time',sig = 0.05):
 # Pearson correlation and pvalue for xarray 
 
 def pcorr(x,y,skipnan = False):
+    '''
+    This function compute pearson correlation for two time series.    
+
+    Parameters
+    ----------
+    x: numpy.array  
+        Array of variable 1 
+    y: numpy.array
+        Array of variable 2 
+    skipna: bool 
+        skip the nan values in both arrays (False is the default)       
+    
+    Returns
+    -------
+    r:  float
+	pearson correlation value
+    '''     
     if not skipnan:        
         xx = x[~np.isnan(x) & ~np.isnan(y)]
         yy = y[~np.isnan(x) & ~np.isnan(y)]
@@ -69,6 +86,7 @@ def pcorr(x,y,skipnan = False):
         yy = y 
     return sts.pearsonr(xx,yy)
 
+### Pearson Correlation for xarray (this function also returns p-value)
 def xr_person(dx,dy,dim = 'time',skipnan = False):
     r,p = xr.apply_ufunc(pcorr,dx,dy,
                          input_core_dims=[[dim],[dim]],                         
@@ -79,3 +97,39 @@ def xr_person(dx,dy,dim = 'time',skipnan = False):
     ds['r'] = r
     ds['pvalue'] = p
     return ds 
+
+
+
+### Test of differnces
+def xr_diff_test(dx,dy,dim = 'time',test = 'T'):
+    if test == 'T':
+        test_func = sts.mannwhitneyu
+    elif test == 'U':
+        test_func = sts.ttest_ind    
+    else:
+        print('The test shoud be T or U')
+    
+    ds = xr.Dataset()
+    stat,pvalue = xr.apply_ufunc(test_func,dx,dy,
+                                 input_core_dims = [[dim],[dim]],
+                                 output_core_dims = [[] for _ in range(2)],
+                                 vectorize = True,
+                                 )     
+    ds['stat'] = stat
+    ds['pvalue'] = pvalue
+    return ds
+
+
+# crop xarray dataset in spatial dimension
+def crop_ds(ds,west=-85,east=-60,north=-10,south=-60,lon_name= 'lon',lat_name = 'lat'):
+    return ds.where((ds[lon_name]>=west)&(ds[lon_name]<=east)&(ds[lat_name]>=south)&(ds[lat_name]<=north),drop = True)
+
+# convert longitude coordinate 0 to 360 into -180 to 180
+def lon360to180(ds,lon_name = 'lon'):
+    ds.coords[lon_name] = (ds.coords[lon_name] + 180) % 360 - 180
+    return ds 
+# Remove seasonal cycle from a dataset
+def remove_seasonal_cycle(ds,time_dim = 'time'):
+    gb = ds.groupby(f'{time_dim}.month')
+    ds_anom = gb - gb.mean(dim=time_dim)
+    return ds_anom
